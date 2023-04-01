0a86219acb803da5b4f0eef994cfe410b8210485
bellehlim <53798540+bellehlim@users.noreply.github.com>
2023-04-01 17:08:29 -0400

    Recipe card and list (#4)
    * Recipe card + list from json
    
    * modified ts config to allow for testing
    
    * revert recipe type for now
    
    * defined key for each child
    
    * pot pie (yum)
    
    ---------
    
    Co-authored-by: lucass0717 <lucasharbani@gmail.com>

---
+++ src/components/recipes/recipe-card.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+const savesArray = require("../../data/recipes/saves.json");
+
+const RecipeCard = (props: any) => {
+  return (
+      <div className="card mb-3" style ={{width: "50rem"}}>
+        <div className="row">
+          <div className="col-4 ">
+            <img src={props.image} className="card-img" alt="..."/>
+          </div>
+          <div className="col-7">
+            <div className="card-body">
+              <h5 className="card-title">{props.title}</h5>
+              <span className="card-text">Cook time: {props.readyInMinutes} minutes</span>
+              <div className="card-text"><small className="text-muted">Tags:</small>{random3Tags(props)}</div>
+              <button type="button" className="btn btn-success">Make a Post</button>
+            </div>
+          </div>
+          <div className="col-1">
+            <button className='btn bi bi-bookmark pull-right'/>
+            <p className="card-text">{findSavesByRecipe(props.id)}</p>
+          </div>
+        </div>
+      </div>
+  );
+};
+export default RecipeCard;
+
+
+function fixFormatting(str: string) {
+  str = str.replace(/[A-Z]/g, (c) => {
+    return " " + c.toLowerCase();
+  });
+  return str;
+}
+
+function findSavesByRecipe(recipe_id: number) {
+  let count = 0;
+  for (let i = 0; i < savesArray.saves.length; i++) {
+    if (savesArray.saves[i].recipe_id === recipe_id) {
+      count++;
+    }
+  }
+  return count;
+}
+
+
+function random3Tags(recipe: any) {
+  const tagsOfInterest = ["vegetarian", "vegan", "glutenFree", "dairyFree", "veryHealthy", "cheap", "veryPopular", "sustainable"];
+  const categoriesOfInterest = ["cuisines", "dishTypes", "diets", "occasions"];
+  let allTags : string[] = [];
+  for (const key in recipe) {
+    if (tagsOfInterest.includes(key)) {
+      allTags.push(fixFormatting(key));
+    }
+    if (categoriesOfInterest.includes(key)) {
+      allTags = allTags.concat(recipe[key]);
+    }
+  }
+  allTags.sort(() => Math.random() - Math.random());
+  const threeTags = allTags.slice(0, 3);
+  return (
+      <div>
+        {
+          threeTags.map(tag =>
+              <span key={tag} className="badge rounded-pill bg-info">{tag}</span>
+          )
+        }
+      </div>);
+}
