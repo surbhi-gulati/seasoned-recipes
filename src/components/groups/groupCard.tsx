a66a3a0aa3b6e3fa0e31d5d35e5144800b9ebc1c
bellehlim@gmail.com <bellehlim@gmail.com>
2023-04-21 17:29:12 -0400

    make main group card clickable

--- src/components/groups/groupCard.tsx
+++ src/components/groups/groupCard.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import GroupType from "../../modules/groupType";
 import {useSelector} from "react-redux";
 import {createGroupMember} from "../../services/group-members-services";
+import {Link} from "react-router-dom";
 
 const GroupCard = (props: GroupType) => {
-
   const {currentUser} = useSelector((state: any) => state.auth);
   const clickPostHandler = async () => {
     try {
@@ -14,23 +14,25 @@ const GroupCard = (props: GroupType) => {
     }
   };
     return (
-    <div className="card my-3">
-      <div className="my-2"> 
-        <img 
-          className="card-img mt-2" 
-          src={`${props.image}`} 
-          alt={props.name} />
-      </div>
-      <div className="row">
-        <div className="card-body col-9">
-          <h5 className="card-title">{props.name}</h5>
-          <p className="card-text">{props.description}</p>
-        </div>
-        <div className="float-end col-2" >
-          { currentUser != null ? <button onClick = {clickPostHandler} className="btn btn-lg btn-primary mt-4 rounded-pill">Join</button> : <p></p>}
-        </div>
-      </div>
-    </div>
+        <Link to={`/group/${props._id}`} style={{color: 'black', textDecoration: 'none' }}>
+          <div className="card my-3">
+            <div className="my-2">
+              <img
+                className="card-img mt-2"
+                src={`${props.image}`}
+                alt={props.name} />
+            </div>
+            <div className="row">
+              <div className="card-body col-9">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">{props.description}</p>
+              </div>
+              <div className="float-end col-2" >
+                { currentUser != null ? <button onClick = {clickPostHandler} className="btn btn-lg btn-primary mt-4 rounded-pill">Join</button> : <p></p>}
+              </div>
+            </div>
+          </div>
+        </Link>
     );
 };
 export default GroupCard;
