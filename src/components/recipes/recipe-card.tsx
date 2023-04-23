a4de3b96e7d3a5f46fdbec54b49a4c3f34f40c7f
Surbhi Gulati <surbhigulati2000@gmail.com>
2023-04-22 22:46:15 -0400

    update bookmark button toggling in recipe card

--- src/components/recipes/recipe-card.tsx
+++ src/components/recipes/recipe-card.tsx
@@ -1,51 +1,91 @@
-import React from "react";
-import {Link, useLocation} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {updateRecipeSaves} from "../../reducers/recipe-reducer";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { updateRecipeSaves } from "../../reducers/recipe-reducer";
 import RecipeType from "../../modules/recipeType";
 const savesArray = require("../../data/recipes/saves");
 
 export const RecipeCard = (props: RecipeType) => {
-  const dispatch = useDispatch()
-  const location = useLocation()
-  const {currentUser} = useSelector((state: any) => state.auth);
-  const showMakePostButton = currentUser != null && (location.pathname.includes("/search") || location.pathname.includes("/recipe"));
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const { currentUser } = useSelector((state: any) => state.auth);
+  const showMakePostButton =
+    currentUser != null &&
+    (location.pathname.includes("/search") ||
+      location.pathname.includes("/recipe"));
+  const [isBookmarked, setIsBookmarked] = useState(false);
   const updateRecipeSavesHandler = (id) => {
+    setIsBookmarked(!isBookmarked);
     dispatch(updateRecipeSaves(id));
-  }
+  };
 
   const numberOfSaves = findSavesByRecipe(props.id);
   return (
-      <div className="row card">
-        <div className="row">
-          <div className="col-4 ">
-            <Link to={`/recipe/${props.id}`} style={{color: 'black', textDecoration: 'none' }}>
-              <img src={props.thumbnail_url} className="card-img" alt="..."/>
+    <div className="row card">
+      <div className="row">
+        <div className="col-4 ">
+          <Link
+            to={`/recipe/${props.id}`}
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <img
+              src={props.thumbnail_url}
+              className="card-img"
+              alt="..."
+            />
+          </Link>
+        </div>
+        <div className="col-7">
+          <div className="card-body">
+            <Link
+              to={`/recipe/${props.id}`}
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <h5 className="card-title">{props.name}</h5>
             </Link>
-          </div>
-          <div className="col-7">
-            <div className="card-body">
-              <Link to={`/recipe/${props.id}`} style={{color: 'black', textDecoration: 'none' }}>
-                <h5 className="card-title">{props.name}</h5>
-              </Link>
-              <span className="card-text">{props.yields}</span>
-              {props.total_time_minutes && <div className="card-text">{`Cook time: ${props.total_time_minutes} minutes`}</div>}
-              <div className="card-text mb-1"><small className="text-muted">Tags: </small>{getTags(props.tags)}</div>
-              {showMakePostButton ? <div><Link to={`/newPost/${props.id}`}><button type="button" className="btn btn-success">Make a Post</button></Link></div> : <i/>}
+            <span className="card-text">{props.yields}</span>
+            {props.total_time_minutes && (
+              <div className="card-text">{`Cook time: ${props.total_time_minutes} minutes`}</div>
+            )}
+            <div className="card-text mb-1">
+              <small className="text-muted">Tags: </small>
+              {getTags(props.tags)}
             </div>
-          </div>
-          <div className="col-1">
-            {numberOfSaves > 2 ?
-                <p onClick = {() => updateRecipeSavesHandler(props._id)} className="bi bi-bookmark-fill float-end">{numberOfSaves}</p> :
-                <p onClick = {() => updateRecipeSavesHandler(props._id)} className="bi bi-bookmark float-end">{numberOfSaves}</p>
-            }
+            {showMakePostButton ? (
+              <div>
+                <Link to={`/newPost/${props.id}`}>
+                  <button type="button" className="btn btn-success">
+                    Make a Post
+                  </button>
+                </Link>
+              </div>
+            ) : (
+              <i />
+            )}
           </div>
         </div>
+        <div className="col-1">
+          {isBookmarked ? (
+            <p
+              onClick={() => updateRecipeSavesHandler(props.id)}
+              className="bi bi-bookmark-fill float-end"
+            >
+              {numberOfSaves}
+            </p>
+          ) : (
+            <p
+              onClick={() => updateRecipeSavesHandler(props.id)}
+              className="bi bi-bookmark float-end"
+            >
+              {numberOfSaves}
+            </p>
+          )}
+        </div>
       </div>
+    </div>
   );
 };
 export default RecipeCard;
-
 
 function findSavesByRecipe(recipe_id: number) {
   let count = 0;
