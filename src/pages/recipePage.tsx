b8af71b2abb4689cba3247d3d036ed9effee1a57
Surbhi Gulati <surbhigulati2000@gmail.com>
2023-04-24 17:34:48 -0400

    fixed instr, still css ing

--- src/pages/recipePage.tsx
+++ src/pages/recipePage.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import { getRecipeInfoByID } from "../services/recipe-api-service";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 const RecipePage = () => {
   const [recipeInfo, setRecipeInfo] = React.useState<any>({});
-  const {recipe_id} = useParams();
-  
+  const { recipe_id } = useParams();
+
   // This gets the recipe info from the API
   const getRecipeInfoHandler = async (recipe_id: number) => {
     const response = await getRecipeInfoByID(recipe_id);
@@ -19,13 +19,13 @@ const RecipePage = () => {
       tags: response.tags,
       total_time_minutes: response.total_time_minutes,
       yields: response.yields,
-      instructions: response.instructions[0]
-    }
+      instructions: response.instructions,
+    };
     setRecipeInfo(recipeInfo);
-  }
+  };
 
   // This gets the current user from the redux store
-  const {currentUser} = useSelector((state: any) => state.auth);
+  const { currentUser } = useSelector((state: any) => state.auth);
   const [user, setUser] = React.useState<any>();
   useEffect(() => {
     setUser(currentUser);
@@ -33,43 +33,41 @@ const RecipePage = () => {
 
   // If the recipe_id is not null, then get the recipe info on page load
   useEffect(() => {
-    if(recipe_id) {
+    if (recipe_id) {
       getRecipeInfoHandler(parseInt(recipe_id));
     }
   }, [recipe_id]);
 
-  // Pass the recipe info to the new post window
   return (
-      <div>
-        <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-          <div className="px-0">
-            <h1 className="display-4 font-italic">{recipeInfo.name}</h1>
-            <p className="lead my-3">{recipeInfo.description}</p>
-          </div>
-        </div>
-
-        <div className="row mb-2">
-          <div className="col-md-5">
-            <div className="card flex-md-row mb-4 box-shadow h-md-250">
-              <div className="card-body d-flex flex-column align-items-start">
-                <b className="mb-0">{recipeInfo.yields}</b>
-                <div className="mb-1 text-muted">Cook time: {recipeInfo.total_time_minutes} minutes</div>
-
-              </div>
-            </div>
-          </div>
-          <div className="col-md-7">
-            <div className="card flex-md-row mb-4 box-shadow h-md-250">
-              <div className="card-body d-flex flex-column align-items-start">
-              </div> <p>{recipeInfo.instructions}</p>
-              {/*{recipeInfo.instructions.forEach((step) => {*/}
-              {/*  return <p>{step}</p>*/}
-              {/*})}*/}
-            </div>
-          </div>
+    <div>
+      <img src={recipeInfo.thumbnail_url} className="card-img" alt="..."/>
+      <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+        <div className="col-md-6 px-0">
+          <h1 className="display-4 font-italic">{recipeInfo.name}</h1>
+          {(recipeInfo.total_time_minutes != null) && <div className="mb-1 text-muted"> {recipeInfo.total_time_minutes} </div>}
+          <div className="mb-1 text-muted">{recipeInfo.yields}</div>
+          <p className="lead my-3">{recipeInfo.description}</p>
+          <div className="card-text mb-1"><small className="text-muted">Tags: </small>{getTags(recipeInfo.tags)}</div>
+          <ul>
+            {recipeInfo.instructions &&
+              recipeInfo.instructions.map((instruction: any, index: number) => (
+                <li key={index}>{instruction.display_text}</li>
+              ))}
+          </ul>
         </div>
       </div>
+    </div>
   );
 };
+
+const getTags = (tags: Array<any>) => {
+  const limit = 3;
+  if(tags) {
+    const limitedTags = tags.slice(0, limit);
+    return limitedTags.map(tag => 
+      <span key={tag.id} className="badge mx-1 rounded-pill bg-info">{`${tag.display_name}`} </span>
+    );
+  }
+}
 
 export default RecipePage;
