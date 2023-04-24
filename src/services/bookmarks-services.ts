0a35bddc849eb67afd33d82fce00d016b42f5b0b
bellehlim <53798540+bellehlim@users.noreply.github.com>
2023-04-24 10:59:48 -0400

    bookmarks/saving a recipe (#24)
    * icons now work to save
    
    * unbookmarking works
    
    * logic for toggling icon
    
    * delete unused reducers

--- src/services/bookmarks-services.ts
+++ src/services/bookmarks-services.ts
@@ -1,15 +1,28 @@
 import axios from "axios";
+import RecipeType from "../modules/recipeType";
+import {createRecipe, getRecipeByID} from "./recipe-services";
 
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 const BOOKMARKS_URL = `${SERVER_API_URL}/bookmarks`;
 
 const api = axios.create({ withCredentials: true });
 
-export const createBookmark = async (bookmark) => {
+export const createBookmark = async (recipeInfo: RecipeType, user: string) => {
   try {
-    const response = await api.post(`${BOOKMARKS_URL}`, bookmark);
-    const newBookmark = response.data;
-    return newBookmark;
+    const existingRecipe = await getRecipeByID(recipeInfo.id);
+    if (!existingRecipe) {
+      await createRecipe(recipeInfo);
+    }
+    const newRecipeObj = await getRecipeByID(recipeInfo.id);
+    const recipe = newRecipeObj._id;
+    if (recipe) {
+      const response = await api.post(`${BOOKMARKS_URL}`, {
+        user, recipe
+      });
+      const newBookmark = response.data;
+      return newBookmark;
+    }
+    return null;
   } catch (error) {
     console.log("error: ", error);
     return null;
@@ -18,7 +31,25 @@ export const createBookmark = async (bookmark) => {
 
 export const getBookmarksByRecipeId = async (recipeId) => {
   try {
-    const response = await api.get(`${BOOKMARKS_URL}/${recipeId}`);
+    const recipeObj = await getRecipeByID(recipeId);
+    const recipeObjId = recipeObj._id;
+    if (recipeObjId) {
+      const response = await api.get(`${BOOKMARKS_URL}/${recipeObjId}`);
+      const bookmarks = response.data;
+      return bookmarks;
+    }
+    return null;
+  } catch (error) {
+    console.log("error: ", error);
+    return null;
+  }
+};
+
+export const getBookmarksByUserId = async (userId, objectsOrRecipes) => {
+  try {
+    const response = objectsOrRecipes ?
+        await api.get(`${BOOKMARKS_URL}/objects/user/${userId}/`) :
+        await api.get(`${BOOKMARKS_URL}/user/${userId}/`);
     const bookmarks = response.data;
     return bookmarks;
   } catch (error) {
@@ -27,22 +58,31 @@ export const getBookmarksByRecipeId = async (recipeId) => {
   }
 };
 
-export const getBookmarksByUserId = async (userId) => {
+export const getBookmarksByBothIds = async (userId, recipeId) => {
   try {
-    const response = await api.get(`${BOOKMARKS_URL}/${userId}`);
-    const bookmarks = response.data;
-    return bookmarks;
+    const recipeObj = await getRecipeByID(recipeId);
+    const recipeObjId = recipeObj._id;
+    if (recipeObjId) {
+      const response = await api.get(`${BOOKMARKS_URL}/${recipeObjId}/${userId}/`);
+      const bookmarks = response.data;
+      return bookmarks;
+    }
+    return null;
   } catch (error) {
     console.log("error: ", error);
     return null;
   }
 };
 
-export const unbookmark = async (bookmark) => {
+export const unbookmark = async (recipeInfo: RecipeType, user: string) => {
   try {
-    const response = await api.delete(`${BOOKMARKS_URL}`, { data: bookmark });
-    const deletedBookmark = response.data;
-    return deletedBookmark;
+    const recipeObj = await getRecipeByID(recipeInfo.id);
+    const recipe = recipeObj._id;
+    if (recipe) {
+      const response = await api.delete(`${BOOKMARKS_URL}`, { data: { user: user, recipe: recipe } });
+      return response.data;
+    }
+    return null;
   } catch (error) {
     console.log("error: ", error);
     return null;
